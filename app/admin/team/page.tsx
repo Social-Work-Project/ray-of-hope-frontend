743ecc335056893f349df15fbbe9f2554b9783e7
Goku216 <sagarpariyar345@gmail.com>
2026-05-20 12:08:35 -0400

    team member logic change

--- app/admin/team/page.tsx
+++ app/admin/team/page.tsx
@@ -266,7 +266,7 @@ export default function AdminTeamPage() {
   const fetchTeams = async () => {
     try {
       const res = await AdminService.getAllTeams();
-      setTeams(res.data.results || []);
+      setTeams((res.data.results || []).slice().reverse());
     } catch {
       toast.error('Failed to fetch team members.');
     }
